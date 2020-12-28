e0ac701066e366ed01f31a02f86f92df5878db4d
dky <dky@atguigu.com>
2020-12-28 15:25:55 +0800

    程序员增加了一个功能模块

---
+++ feature-game/src/main/java/Game.java
@@ -0,0 +1,11 @@
+/**
+ * @Author : dky
+ * @Version : 1.0
+ * @CreateDate : 2020/12/28 15:23
+ * @Description :
+ */
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("超级玛丽");
+    }
+}
