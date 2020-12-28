6f5769f1ab1f7553a4bf82f233e3317d4d014783
dky <dky@atguigu.com>
2020-12-28 15:14:55 +0800

    项目经理创建了项目

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Author : dky
+ * @Version : 1.0
+ * @CreateDate : 2020/12/28 15:07
+ * @Description :
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("编程真好玩啊");
+    }
+}
